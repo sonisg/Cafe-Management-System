dd4e2867ee2fe909e33b5f0f9148fe583341d131
sonisg <38506968+sonisg@users.noreply.github.com>
2023-07-01 20:57:22 +0530

    Delete Cafe directory

--- Cafe/src/main/java/com/in/cafe/service/DashBoardService.java
+++
@@ -1,9 +0,0 @@
-package com.in.cafe.service;
-
-import org.springframework.http.ResponseEntity;
-
-import java.util.Map;
-
-public interface DashBoardService {
-    ResponseEntity<Map<String, Object>> getCount();
-}
