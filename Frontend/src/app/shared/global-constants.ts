72ddf10d9bda0a8056954ae582ffffab6ef955d3
sonisg <38506968+sonisg@users.noreply.github.com>
2023-08-11 17:37:38 +0530

    Add files via upload

---
+++ Frontend/src/app/shared/global-constants.ts
@@ -0,0 +1,10 @@
+export class GlobalConstants{
+    public  static genericError: string = "Something Went wrong. Please try again later";
+    public static nameRegex: string = "[a-zA-Z0-9 ]*";
+    public static emailRegex: string = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
+    public static contactNumberRegex: string = "^[e0-9]{10,10}$";
+    public static error: string = "error";
+    public static  unauthorized: string ="You are not authorised to access the page."
+    public static productExistError: string = "Product already exists."
+    public static productAdded: string = "Product added successfully."
+}
